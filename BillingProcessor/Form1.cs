accd1c9b593c2dda0bebd2e5ef0c39e4787b5ecc
stashuk <stashuk@yahoo.com>
2016-05-06 12:46:54 -0400

    Added SpecificFormulas update

--- BillingProcessor/Form1.cs
+++ BillingProcessor/Form1.cs
@@ -17,6 +17,8 @@ namespace BillingProcessor
 
         public bool formulasInserted;
         public float Sum = 0;
+        public int newTransactionFeeLine;
+        public int newMonthlyFeeLine;
 
         public Form1()
         {
@@ -102,8 +104,6 @@ namespace BillingProcessor
 
             int rCnt = 0;
 
-            //string filepath = @"C:\Users\sshatkin\Documents\Billing\Client Templates\Alaska Federal Credit Union\Cor360BillingTemplate - Alaska Federal Credit Union.xlsx";
-
             xlApp = new Excel.Application();
             xlWorkBook = xlApp.Workbooks.Open(filepath, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
@@ -113,6 +113,11 @@ namespace BillingProcessor
             }
 
             range = xlWorkSheet.UsedRange;
+
+            if (insertType == "specific_formula")
+            {
+                updateSpecficFormula(ref xlWorkSheet, ref range, true, true);
+            }
 
             for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
             {
@@ -148,19 +153,6 @@ namespace BillingProcessor
                             }
                         }
 
-                        //if (range.Cells[rCnt, 6].Value2.ToString().Trim() == "Email Monthly Fee")
-                        //{
-                        //    //insert next new row
-                        //    Excel.Range Line = (Excel.Range)xlWorkSheet.Rows[rCnt + 1];
-                        //    Line.Insert();
-                        //    range.Cells[rCnt + 1, 2].Value2 = "Single Sign-On Monthly Fee";
-                        //    range.Cells[rCnt + 1, 6].Value2 = "Single Sign-On Monthly Fee";
-                        //    range.Cells[rCnt + 1, 7].Value2 = 0;
-                        //    range.Cells[rCnt + 1, 8].Value2 = "Specify Single Sign-On Monthly Fee";
-                        //    xlWorkSheet.Range[range.Cells[rCnt + 1, 2], range.Cells[rCnt + 1, 3]].Merge();
-
-                        //    //break;
-                        //}
                     }
                 }
                 else if (insertType == "formulas")
@@ -179,8 +171,6 @@ namespace BillingProcessor
                         }
                     }
                 }
-
-
             }
 
             xlApp.DisplayAlerts = false;
@@ -218,7 +208,7 @@ namespace BillingProcessor
         {
             cycleThroughFiles("schedule");
             cycleThroughFiles("formulas");
-            cycleThroughFiles("one_formula");
+            cycleThroughFiles("specific_formula");
         }
 
         private void updateFormulas(ref Excel.Worksheet xlWorkSheet, ref Excel.Range range, int Position, int offset)
@@ -237,6 +227,8 @@ namespace BillingProcessor
             //more formulas
             range.Cells[Position, 3].Formula = range.Cells[Position, 4].Formula.Replace(range.Cells[Position, 4].Formula.ToString().Substring(range.Cells[Position, 4].Formula.ToString().IndexOf("),") + 3, 8), "").Replace(", 0", ",' '");
             range.Cells[Position, 2].Formula = range.Cells[Position, 4].Formula.Replace(range.Cells[Position, 4].Formula.ToString().Substring(range.Cells[Position, 4].Formula.ToString().IndexOf("*"), 6), "");
+
+            newTransactionFeeLine = Position;
         }
 
         private void btnModifyOneFormula_Click(object sender, EventArgs e)
@@ -244,92 +236,79 @@ namespace BillingProcessor
             cycleThroughFiles("one_formula");
         }
 
-        private void updateFormula(string filepath)
+        private void updateSpecficFormula(ref Excel.Worksheet xlWorkSheet, ref Excel.Range range, bool processMonthly, bool processTransaction)
         {
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            Excel.Range range;
+            if (processMonthly || processTransaction)
+            {               
+                int rCnt = 0;
+
+                //Expense SaaS
+                int iExpenseSaaSDesc = findElement(ref xlWorkSheet, ref range, "Expense Report SaaS fee", 6);
+                int iExpenseSaaSLine = findElement(ref xlWorkSheet, ref range, "F" + iExpenseSaaSDesc.ToString(), 1);
 
 
-            int rCnt = 0;            
+                int iSSOSDesc = findElement(ref xlWorkSheet, ref range, "Single Sign-On Monthly Fee", 6);
+                int iSSOLine = findElement(ref xlWorkSheet, ref range, "F" + iSSOSDesc.ToString(), 1);
 
-            //string filepath = @"C:\Users\sshatkin\Documents\Billing\Client Templates\Alaska Federal Credit Union\Cor360BillingTemplate - Alaska Federal Credit Union.xlsx";
 
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Open(filepath, 0, false, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            //xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Sheets["August-2015 Invoice"];
-            //if (xlWorkSheet.ProtectContents == true)
-            //{
-            //    xlWorkSheet.Unprotect("c0r36o");
-            //}
+                for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
+                {
+                    if (range.Cells[rCnt, 1].Value2 != null)
+                    {
+                        if (processMonthly)
+                        { 
+                            if (range.Cells[rCnt, 1].Formula.ToString().Trim().IndexOf("Monthly Fees") > 0)
+                            {
+                                range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.Replace(" =", "=").Replace("= ", "=");
+                                range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.ToString().Replace("A" + (iExpenseSaaSLine - 1).ToString() + "=\" \",", "A" + (iExpenseSaaSLine - 1).ToString() + "=\" \"," + " A" + (iExpenseSaaSLine).ToString() + "=\" \",");
+                                range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.ToString().Replace("A" + (iSSOLine - 1).ToString() + "=\" \")", "A" + (iSSOLine - 1).ToString() + "=\" \"," + " A" + (iSSOLine).ToString() + "=\" \")");
+                                //range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.ToString().Replace("A" + (iSSOLine - 1).ToString() + " = \" \",", "A" + (iSSOLine - 1).ToString() + " = \" \"," + " A" + (iSSOLine).ToString() + " = \" \",");
 
-            range = xlWorkSheet.UsedRange;
+                                processMonthly = false;
+                            }
+                        }
+                    
+                        if (processTransaction)
+                        { 
+                            if (range.Cells[rCnt, 1].Formula.ToString().Trim().IndexOf("Transaction Fees") > 0)
+                            {
+                                range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.Replace(" =", "=").Replace("= ", "=");
+                                range.Cells[rCnt, 1].Formula = range.Cells[rCnt, 1].Formula.ToString().Replace("A" + (newTransactionFeeLine - 1).ToString() + "=\" \",", "A" + (newTransactionFeeLine - 1).ToString() + "=\" \"," + " A" + (newTransactionFeeLine).ToString() + "=\" \",");
+
+                                processTransaction = false;
+                            }
+                        }
+
+                        if (!(processMonthly || processTransaction))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+            
+            }
+        }
+
+        private int findElement (ref Excel.Worksheet xlWorkSheet, ref Excel.Range range, string findString, int col)
+        {
+            int rCnt = 0,
+                iFound = 0;
 
             for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
             {
-
-                //// changed formula from old to new
-                //if (range.Cells[rCnt, 4].Formula != null)
-                //{
-                //    if (range.Cells[rCnt, 4].Formula.ToString().Trim() == "=SUM(D16:D40)")
-                //    {
-                //        range.Cells[rCnt, 4].Formula = "=SUM(D16:D41)";
-
-                //        break;
-                //    }
-                //}
-
-                //// check where new formula doesn't exist
-                //if (range.Cells[rCnt, 4].Formula.ToString().Trim() == "=SUM(D16:D41)")
-                //{
-                //    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"FormulaExists.txt", true))
-                //    {
-                //        file.AutoFlush = true;
-                //        file.WriteLine(filepath);
-                //    }
-
-                //    break;
-                //}
-
-                if (range.Cells[rCnt, 1].Value2 != null)
+                if (range.Cells[rCnt, col].Value2 != null)
                 {
-                    if (range.Cells[rCnt, 1].Value2.ToString().Trim() == "INVOICE TOTAL: USD")
-                    {
-                        Sum += range.Cells[rCnt, 4].Value2;
 
+                    if (range.Cells[rCnt, col].Formula.ToString().Trim().IndexOf(findString) > -1)
+                    {
+                        iFound = rCnt;
                         break;
                     }
                 }
 
             }
-            
-                //if ((range.Cells[151, 1].Value2.ToString().Trim() == "Billing Month"))
-                //{
-                //    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"DataAligned.txt", true))
-                //    {
-                //        file.AutoFlush = true;
-                //        file.WriteLine(filepath);
-                //    }                    
-                //}  
-
-            xlApp.DisplayAlerts = false;
-
-            //if (xlWorkSheet.ProtectContents == false)
-            //{
-            //    xlWorkSheet.Protect("c0r36o");
-            //}
-
-            //xlWorkBook.Save();
-
-
-            xlWorkBook.Close(true, null, null);
-            xlApp.Quit();
-
-            releaseObject(xlWorkSheet);
-            releaseObject(xlWorkBook);
-            releaseObject(xlApp);
+            return iFound;
         }
     }
 }
